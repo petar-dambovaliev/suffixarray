931b5cadd5177f16c2213b311d34c9c62f5d819b
petar-dambovaliev <petar.atanasov.1987@gmail.com>
2019-01-05 18:41:19 +0100

    use lcp array and add test
    Update README.md
    
    Update README.md
    
    add tests
    
    Update README.md
    
    Update README.md
    
    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# suffixarray
+# Suffix Array
+This package provides common functionality associated with suffix arrays.
+
+```
+//returns [][]byte of all distinct substrings of "azaza"
+sa := suffix.NewArray([]byte("azaza"))
+sub := sa.DistinctSub()
+
+for k, v := range sub {
+		fmt.Printf("%+v\n", string(vv))
+}
+
+a
+az
+aza
+azaz
+azaza
+z
+za
+zaz
+zaza
+
+//returns int count of all distinct substrings 
+distinctSubCount := sa.DistinctSubCount()
+println(distinctSubCount)
+9
+
+//returns int count of all possible substrings 
+subcount := sa.SubCount()
+println(subcount)
+15```
--- array_test.go
+++ array_test.go
@@ -2,7 +2,6 @@ package suffix
 
 import (
 	"bytes"
-	"fmt"
 	"math/rand"
 	"testing"
 	"time"
@@ -13,15 +12,62 @@ func BenchmarkArray_DistinctSub(b *testing.B) {
 	var bb bytes.Buffer
 	rand.Seed(time.Now().UnixNano())
 
-	for i := 0; i < 15000; i++ {
+	for i := 0; i < 5000; i++ {
 		bb.WriteRune(rune(rand.Intn(26) + 97))
 	}
 	str := bb.String()
 	b.StartTimer()
-	sa := NewArray(str)
+	sa := NewArray([]byte(str))
 
 	for i := 0; i < b.N; i++ {
 		sa.DistinctSub()
 	}
 	b.StopTimer()
+}
+
+func TestArray_DistinctSub(t *testing.T) {
+	sa := NewArray([]byte("azaza"))
+	sub := sa.DistinctSub()
+
+	if len(sub) != 9 {
+		t.Errorf("Distinct substrings should be [a az aza azaz azaza z za zaz zaza]   %+v returned\n", sub)
+	}
+
+	r := [][]byte{
+		{97},
+		{97, 122},
+		{97, 122, 97},
+		{97, 122, 97, 122},
+		{97, 122, 97, 122, 97},
+		{122},
+		{122, 97},
+		{122, 97, 122},
+		{122, 97, 122, 97},
+	}
+
+	for kk, vv := range sub {
+		for k, v := range vv {
+			if v != r[kk][k]{
+				t.Errorf("substring %v should be %v %v given", kk, string(vv), string(r[kk]))
+			}
+		}
+	}
+}
+
+func TestArray_DistinctSubCount(t *testing.T) {
+	sa := NewArray([]byte("azaza"))
+	c := sa.DistinctSubCount()
+
+	if c != 9 {
+		t.Errorf("expected 9 got %v\n", c)
+	}
+}
+
+func TestArray_SubCount(t *testing.T) {
+	sa := NewArray([]byte("azaza"))
+	c := sa.SubCount()
+
+	if c != 15 {
+		t.Errorf("expected 15 got %v\n", c)
+	}
 }
